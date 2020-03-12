60f76d921fcc48970c5b93ed022434f5e00f7fb8
Leonid <ski20173@yandex.ru>
2020-03-12 16:28:00 +0300

    Added to allowed hosts local adress

--- base/settings.py
+++ base/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['kkmt.herokuapp.com']
+ALLOWED_HOSTS = ['kkmt.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
